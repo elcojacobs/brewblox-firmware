8f7d23bd28393cb0473fbfcb12f1aa8e0fc35f12
Bob Steers <steers.bob@gmail.com>
2019-01-03 18:27:05 +0100

    also create ARM32v7 image of flasher

--- docker/build-particle.sh
+++ docker/build-particle.sh
@@ -4,4 +4,7 @@ set -e
 # The particle image is expected to remain relatively stable
 # It wraps the Particle CLI in a docker image, so it can easily be used to flash the firmware
 
-docker build -t brewblox/firmware-particle particle
+docker run --rm --privileged multiarch/qemu-user-static:register --reset
+
+docker build -t brewblox/firmware-particle:latest particle
+docker build -t brewblox/firmware-particle:rpi-latest rpi-particle
---
+++ docker/push-rpi-local.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+set -e
+
+# The build scripts generate :local tags of images
+# This script retags and pushes those images
+#
+# First argument should always be the docker repo name
+# Second argument is the tag name they should be pushed as
+# Leave this blank to set it to the git branch name
+
+CLEAN_BRANCH_NAME=rpi-$(echo "$(git rev-parse --abbrev-ref HEAD)" | tr '/' '-' | tr '[:upper:]' '[:lower:]');
+REPO=$1
+TAG=${2:-${CLEAN_BRANCH_NAME}}
+
+docker tag ${REPO}:rpi-local ${REPO}:${TAG}
+docker push ${REPO}:${TAG}
