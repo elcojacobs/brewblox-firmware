2ea4916e2cd9b08d71a09442c976a4d958bef04d
Elco Jacobs <elco@brewpi.com>
2019-01-07 14:45:09 +0100

    fix app test for integral value

--- app/brewblox/test/PidBlock_test.cpp
+++ app/brewblox/test/PidBlock_test.cpp
@@ -152,5 +152,5 @@ SCENARIO("A Blox Pid object can be created from streamed protobuf data")
                                         "enabled: true active: true "
                                         "kp: 40960 ti: 2000 td: 200 "
                                         "p: 40950 i: 20475 "
-                                        "error: 4095 integral: 4096000");
+                                        "error: 4095 integral: 4095000");
 }
