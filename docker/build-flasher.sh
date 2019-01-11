9dbb471dfc5b04906c56081a93f168d331d908c3
Elco Jacobs <elco@brewpi.com>
2019-01-11 18:36:32 +0100

    prevent old binaries somehow ending up in flasher image

--- docker/build-flasher.sh
+++ docker/build-flasher.sh
@@ -13,15 +13,15 @@ docker run \
         bash compile-proto.sh
         make clean APP=brewblox PLATFORM=p1
         make APP=brewblox PLATFORM=p1
-        cp -r /firmware/build/target/brewblox-p1 /firmware/docker/flasher/brewblox-p1
-        cp -r /firmware/build/target/brewblox-p1 /firmware/docker/rpi-flasher/brewblox-p1
+        cp /firmware/build/target/brewblox-p1/brewblox.bin /firmware/docker/flasher/brewblox-p1.bin
+        cp /firmware/build/target/brewblox-p1/brewblox.bin /firmware/docker/rpi-flasher/brewblox-p1.bin
         make clean APP=brewblox PLATFORM=photon
         make APP=brewblox PLATFORM=photon
-        cp -r /firmware/build/target/brewblox-photon /firmware/docker/flasher/brewblox-photon
-        cp -r /firmware/build/target/brewblox-photon /firmware/docker/rpi-flasher/brewblox-photon
+        cp /firmware/build/target/brewblox-photon/brewblox.bin /firmware/docker/flasher/brewblox-photon.bin
+        cp /firmware/build/target/brewblox-photon/brewblox.bin /firmware/docker/rpi-flasher/brewblox-photon.bin
     '
 
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
 
-docker build -t brewblox/firmware-flasher:local flasher
-docker build -t brewblox/firmware-flasher:rpi-local rpi-flasher
+docker build --no-cache -t brewblox/firmware-flasher:local flasher
+docker build --no-cache -t brewblox/firmware-flasher:rpi-local rpi-flasher
