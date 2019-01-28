7ff14d4a5d049abbd921d92f2b0f35ff581571bb
Elco Jacobs <elco@brewpi.com>
2019-01-28 18:43:39 +0100

    use docker-compose exec and re-use previous build results in ci

--- docker/build-flasher.sh
+++ docker/build-flasher.sh
@@ -1,22 +1,14 @@
 #! /usr/bin/env bash
 set -e
 
-docker run \
-    --rm \
-    --volume $(pwd)/../:/firmware \
-    --name flasher-compiler \
-    brewblox/firmware-compiler \
+docker-compose exec compiler \
     bash -c '
-        set -e
-        cd /firmware/build
-        rm -rf /firmware/docker/simulator/target
+        set -e        
         bash compile-proto.sh
-        make clean APP=brewblox PLATFORM=p1
         make -j APP=brewblox PLATFORM=p1
-        cp /firmware/build/target/brewblox-p1/brewblox.bin /firmware/docker/flasher/brewblox-p1.bin
-        make clean APP=brewblox PLATFORM=photon
+        cp target/brewblox-p1/brewblox.bin ../docker/flasher/brewblox-p1.bin
         make -j APP=brewblox PLATFORM=photon
-        cp /firmware/build/target/brewblox-photon/brewblox.bin /firmware/docker/flasher/brewblox-photon.bin
+        cp target/brewblox-photon/brewblox.bin ../docker/flasher/brewblox-photon.bin
     '
 
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
--- docker/build-simulator.sh
+++ docker/build-simulator.sh
@@ -1,19 +1,13 @@
 #! /usr/bin/env bash
 set -e
 
-docker run \
-    --rm \
-    --volume $(pwd)/../:/firmware \
-    --name simulator-compiler \
-    brewblox/firmware-compiler \
+docker-compose exec compiler \
     bash -c '
-        set -e
-        cd /firmware/build
-        rm -rf /firmware/docker/simulator/target
+        set -e        
+        rm -rf ../docker/simulator/target
         bash compile-proto.sh
-        make clean APP=brewblox PLATFORM=gcc
         make -j APP=brewblox PLATFORM=gcc
-        cp -r /firmware/build/target /firmware/docker/simulator/target
+        cp -r target ../docker/simulator/target
     '
 
 docker build -t brewblox/firmware-simulator:local simulator
