6cf5734a57073bf0a453e33f0c4e35c6d7c491e3
Elco Jacobs <elco@brewpi.com>
2019-02-05 13:10:00 +0100

    run compiler container with GUID/UID from host

--- docker/start-compiler.sh
+++ docker/start-compiler.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 touch .env
 ENV_MOUNTDIR="$(grep MOUNTDIR .env)"
+
 if [ -z "$ENV_MOUNTDIR" ]; then
   MOUNTDIR=$(dirname "$(pwd)")
   echo "Configuring '$MOUNTDIR' as mount path in container"
@@ -22,6 +23,23 @@ if [ -z "$ENV_MAKE_ARGS" ]; then
   echo "MAKE_ARGS=$MAKE_ARGS" >> .env
 else
   echo "Using make args '$ENV_MAKE_ARGS'"
+fi
+
+ENV_UID="$(grep DOCKER_UID .env)"
+ENV_GID="$(grep DOCKER_GID .env)"
+if [ -z "$ENV_UID" ]; then
+  DOCKER_UID=$(id -u)
+  echo "Configuring DOCKER_UID=$DOCKER_UID"
+  echo "DOCKER_UID=$DOCKER_UID" >> .env
+else
+  echo "Using $ENV_UID"
+fi
+if [ -z "$ENV_GID" ]; then
+  DOCKER_GID=$(id -u)
+  echo "Configuring DOCKER_GID=$DOCKER_GID"
+  echo "DOCKER_GID=$DOCKER_GID" >> .env
+else
+  echo "Using $ENV_GID"
 fi
 
 docker-compose up -d compiler
