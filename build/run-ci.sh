942bfe07c81f425a187b177bdd1aa8fd27fc1347
Bob Steers <steers.bob@gmail.com>
2019-03-11 16:37:34 +0100

    simplify run-ci

--- build/run-ci.sh
+++ build/run-ci.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
+set -e
 
-bash build-modules.sh; (( exit_status = exit_status || $? ))
-bash compile-proto.sh; (( exit_status = exit_status || $? ))
-bash build-tests.sh; (( exit_status = exit_status || $? ))
-bash run-tests.sh; (( exit_status = exit_status || $? ))
-bash build-firmware.sh; (( exit_status = exit_status || $? ))
-
-exit $exit_status
+bash compile-proto.sh
+bash build-tests.sh
+bash run-tests.sh
