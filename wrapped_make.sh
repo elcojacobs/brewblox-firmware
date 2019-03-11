46f0cc5bcaf17c79c77782818b2412521326ec1b
Elco Jacobs <elco@brewpi.com>
2019-03-11 18:03:28 +0100

    no error if file to remove doesn't exist (compile_commands.json)

--- wrapped_make.sh
+++ wrapped_make.sh
@@ -4,7 +4,7 @@ MY_DIR=$(dirname "$(readlink -f "$0")")
 
 shopt -s globstar
 compiledb make "$MAKE_ARGS" "$@"
-rm "$MY_DIR/compile_commands.json"
+rm -f "$MY_DIR/compile_commands.json"
 pushd "$MY_DIR" > /dev/null
 cat ./**/compile_commands.json > compile_commands.json \
   && sed -i -e ':a;N;$!ba;s/\]\n\n\[/,/g' compile_commands.json
