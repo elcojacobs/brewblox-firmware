cc4a26aa5667d0964601e54acc4ae680114f3d3a
Elco Jacobs <elco@brewpi.com>
2019-03-11 21:20:01 +0100

    wrapped_make.sh minor edit

--- wrapped_make.sh
+++ wrapped_make.sh
@@ -3,11 +3,12 @@
 MY_DIR=$(dirname "$(readlink -f "$0")")
 
 shopt -s globstar
+# shellcheck disable=SC2068,SC2086
 compiledb make $MAKE_ARGS $@
 rm -f "$MY_DIR/compile_commands.json"
 pushd "$MY_DIR" > /dev/null
-cat ./**/compile_commands.json > compile_commands.json \
-  && sed -i -e ':a;N;$!ba;s/\]\n\n\[/,/g' compile_commands.json
+cat ./**/compile_commands.json > compile_commands.json
+sed -i -e ':a;N;$!ba;s/\]\n\n\[/,/g' compile_commands.json
 chmod 777 compile_commands.json
 popd > /dev/null
 rm "compile_commands.json"
