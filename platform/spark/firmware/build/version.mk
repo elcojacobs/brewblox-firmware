617fd0cc6d2657089e2ed52dfa682edda1dee60b
Elco Jacobs <elco@brewpi.com>
2019-03-15 14:09:53 +0100

    remove particle firmware to re-add as submodule

--- platform/spark/firmware/build/version.mk
+++
@@ -1,8 +0,0 @@
-
-VERSION_STRING = 1.0.0
-
-# PRODUCT_FIRMWARE_VERSION reported by default
-# FIXME: Unclear if this is used, PRODUCT_FIRMWARE_VERSION defaults to 65535 every release
-VERSION = 1000
-
-CFLAGS += -DSYSTEM_VERSION_STRING=$(VERSION_STRING)
