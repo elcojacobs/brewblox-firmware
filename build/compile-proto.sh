49a685635373cef18103eec0590b40cc97d8b3dc
Elco Jacobs <elco@brewpi.com>
2019-04-25 16:25:12 +0200

    use trap to handle proto compile errors

--- build/compile-proto.sh
+++ build/compile-proto.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 MY_DIR=$(dirname "$(readlink -f "$0")")
 
-function status()
-{
-if [[ "$1" -eq 0 ]]; then
-  echo "✓ SUCCESS"
-else
-  echo "✗ FAILED"
-fi
+handle_error() {
+  echo "Encountered error when executing $(basename $0)!"
+  exit 1
 }
+trap handle_error ERR
 
 pushd "$MY_DIR/../app/brewblox/proto" > /dev/null
-echo "Compiling proto files using nanopb for brewblox firmware"
-bash generate_proto_cpp.sh;
-(( result = $? ))
-status $result
-(( exit_status = exit_status || result ))
+echo -e "Compiling proto files using nanopb for brewblox firmware"
+. generate_proto_cpp.sh;
 
-echo "Compiling proto files using google protobuf for unit tests"
-bash generate_proto_test_cpp.sh;
-(( result = $? ))
-status $result
-(( exit_status = exit_status || result ))
+echo -e "Compiling proto files using google protobuf for unit tests"
+. generate_proto_test_cpp.sh;
 popd > /dev/null
 
-exit $exit_status
+echo "Done"
+exit
