1f293cfc00b927d6ae75f5a939cce94e7b5d85ac
Elco Jacobs <elco@brewpi.com>
2019-06-04 15:22:46 +0200

    revert minimal app

--- app/minimal/application.cpp
+++ app/minimal/application.cpp
@@ -1,61 +1,37 @@
 /* Includes ------------------------------------------------------------------*/
 #include "application.h"
-#include <vector>
 
-SYSTEM_THREAD(ENABLED);
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
+// Define the pins we're going to call pinMode on
+int act1 = A0;
+int act2 = A1;
+int act3 = A6;
+int act4 = A7;
 int buzz = A2;
-
-IPAddress ip = uint32_t(0);
-bool connected = false;
-
-void
-printWiFiIp(char dest[16])
-{
-    snprintf(dest, 16, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
-}
-
-std::vector<system_event_t> events;
-
-void
-handleNetworkEvent(system_event_t event, int param)
-{
-    switch (param) {
-    case network_status_connected:
-        ip = WiFi.localIP();
-        connected = true;
-        Particle.connect();
-        break;
-    default:
-        ip = uint32_t(0);
-        connected = false;
-        break;
-    }
-    events.push_back(param);
-}
+const bool invertBuzzer = false;
 
 void
 setup()
 {
+    pinMode(act1, OUTPUT);
+    pinMode(act2, OUTPUT);
+    pinMode(act3, OUTPUT);
+    pinMode(act4, OUTPUT);
     pinMode(buzz, OUTPUT);
-    digitalWrite(buzz, HIGH);
+    digitalWrite(buzz, !invertBuzzer);
     delay(200);
-    digitalWrite(buzz, LOW);
-    Serial.begin();
+    digitalWrite(buzz, invertBuzzer);
 }
 
 void
 loop()
 {
-    char ipString[16];
-    printWiFiIp(ipString);
-    Serial.println();
-    Serial.print("IP: ");
-    Serial.println(ipString);
-    delay(5000);
-    for (auto& event : events) {
-        Serial.print(uint32_t(event));
-        Serial.print(',');
+    int actuators[4] = {act1, act2, act3, act4};
+    for (int i = 0; i < 4; i++) {
+        digitalWrite(actuators[i], HIGH);
+        delay(500);
+        digitalWrite(actuators[i], LOW);
+        delay(500);
     }
 }
