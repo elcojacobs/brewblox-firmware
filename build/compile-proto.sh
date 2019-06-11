bdffa410312c3b0effb2c275517045e5ca86dd2f
Elco Jacobs <elco@brewpi.com>
2019-06-11 14:00:01 +0200

    better error messages when compiling proto

--- build/compile-proto.sh
+++ build/compile-proto.sh
@@ -2,17 +2,18 @@
 MY_DIR=$(dirname "$(readlink -f "$0")")
 
 handle_error() {
-  echo "Encountered error when executing $(basename $0)!"
+  echo "Encountered error when executing $(basename $0)!" >&2
+  echo "Error on line $(caller)" >&2
   exit 1
 }
 trap handle_error ERR
 
 pushd "$MY_DIR/../app/brewblox/proto" > /dev/null
 echo -e "Compiling proto files using nanopb for brewblox firmware"
-. generate_proto_cpp.sh;
+bash generate_proto_cpp.sh;
 
 echo -e "Compiling proto files using google protobuf for unit tests"
-. generate_proto_test_cpp.sh;
+bash generate_proto_test_cpp.sh;
 popd > /dev/null
 
 echo "Done"
