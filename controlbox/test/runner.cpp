fa5a328ba9bbe71efdabb53d2699fecc637ba94f
Elco Jacobs <elco@brewpi.com>
2019-07-09 12:12:08 +0200

    add application command injection in cbox

--- controlbox/test/runner.cpp
+++ controlbox/test/runner.cpp
@@ -1,7 +1,9 @@
 #define CATCH_CONFIG_MAIN
 #define CATCH_CONFIG_CONSOLE_WIDTH 300 // workaround for compatiblity with vscode Test Explorer
 
+#include "CboxError.h"
 #include "Connections.h"
+#include "DataStream.h"
 #include "testinfo.h"
 #include <catch.hpp>
 
@@ -11,11 +13,27 @@ void
 handleReset(bool)
 {
     ++testInfo.rebootCount;
-};
+}
 
 namespace cbox {
 void
 connectionStarted(DataOut& out)
 {
 }
+
+bool
+applicationCommand(uint8_t cmdId, DataIn& in, HexCrcDataOut& out)
+{
+
+    switch (cmdId) {
+    case 100:
+        in.spool();
+        out.writeResponseSeparator();
+        out.write(asUint8(CboxError::OK));
+        out.write(100);
+        return true;
+    default:
+        return false;
+    }
 }
+} // end namespace cbox
