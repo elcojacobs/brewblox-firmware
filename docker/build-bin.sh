f23c19841bcbe855e7931e6a4c6f1b1da4e6bbaf
Bob Steers <steers.bob@gmail.com>
2019-07-17 14:27:15 +0200

    Add system binaries to firmware-bin

--- docker/build-bin.sh
+++ docker/build-bin.sh
@@ -1,22 +1,34 @@
 #! /usr/bin/env bash
 set -e
 
-MY_DIR=$(dirname $(readlink -f $0))
+PARTICLE_VERSION=1.2.1-rc.2
+PARTICLE_RELEASES=https://github.com/particle-iot/device-os/releases/download/v${PARTICLE_VERSION}
 
-rm -rf ./firmware-bin/source || true
-mkdir ./firmware-bin/source
+MY_DIR=$(dirname $(readlink -f $0))
+OUT_DIR="./firmware-bin/source"
+
+rm -rf "${OUT_DIR}" || true
+mkdir "${OUT_DIR}"
 
 docker-compose exec -T compiler \
     bash -c '
         set -e
         bash compile-proto.sh
         make $MAKE_ARGS APP=brewblox PLATFORM=p1
-        cp target/brewblox-p1/brewblox.bin ../docker/firmware-bin/source/brewblox-p1.bin
         make $MAKE_ARGS APP=brewblox PLATFORM=photon
-        cp target/brewblox-photon/brewblox.bin ../docker/firmware-bin/source/brewblox-photon.bin
     '
 
-FILE="./firmware-bin/source/firmware.ini"
+cp ../build/target/brewblox-p1/brewblox.bin "${OUT_DIR}"/brewblox-p1.bin
+curl -sL -o "${OUT_DIR}"/bootloader-p1.bin ${PARTICLE_RELEASES}/p1-bootloader@${PARTICLE_VERSION}.bin
+curl -sL -o "${OUT_DIR}"/system-part1-p1.bin ${PARTICLE_RELEASES}/p1-system-part1@${PARTICLE_VERSION}.bin
+curl -sL -o "${OUT_DIR}"/system-part2-p1.bin ${PARTICLE_RELEASES}/p1-system-part2@${PARTICLE_VERSION}.bin
+
+cp ../build/target/brewblox-photon/brewblox.bin "${OUT_DIR}"/brewblox-photon.bin
+curl -sL -o "${OUT_DIR}"/bootloader-photon.bin ${PARTICLE_RELEASES}/photon-bootloader@${PARTICLE_VERSION}.bin
+curl -sL -o "${OUT_DIR}"/system-part1-photon.bin ${PARTICLE_RELEASES}/photon-system-part1@${PARTICLE_VERSION}.bin
+curl -sL -o "${OUT_DIR}"/system-part2-photon.bin ${PARTICLE_RELEASES}/photon-system-part2@${PARTICLE_VERSION}.bin
+
+FILE="${OUT_DIR}"/firmware.ini
 touch $FILE
 
 echo "[FIRMWARE]" >> $FILE
@@ -24,5 +36,6 @@ echo "firmware_version=$(git rev-parse --short HEAD)" >> $FILE
 echo "firmware_date=$(git show -s --format=%ci)" >> $FILE
 echo "proto_version=$(cd $MY_DIR/../app/brewblox/proto; git rev-parse --short HEAD)" >> $FILE
 echo "proto_date=$(cd $MY_DIR/../app/brewblox/proto; git show -s --format=%ci)" >> $FILE
+echo "system_version=${PARTICLE_VERSION}" >> $FILE
 
 docker build --no-cache -t brewblox/firmware-bin:local firmware-bin
