37bb9f02f6fa53d785b8ad30ea481624d6512d8a
Elco Jacobs <elco@brewpi.com>
2019-12-17 17:36:03 +0100

    add wiring includes to app test build

--- platform/spark/modules/Board/Board.cpp
+++ platform/spark/modules/Board/Board.cpp
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with BrewPi.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "spark_wiring_constants.h"
 
 #include "Board.h"
 #include "delay_hal.h"
