755dbbf50798b85de903c6ebd4e4c115ae0b80c3
Bob Steers <steers.bob@gmail.com>
2020-03-24 18:19:00 +0100

    copy bin fixes

--- docker/copy-bin.sh
+++ docker/copy-bin.sh
@@ -5,15 +5,13 @@ PARTICLE_TAG="v1.5.0-rc.2" # temporary workaround instead of $(git --git-dir ../
 PARTICLE_RELEASES=https://github.com/particle-iot/device-os/releases/download/${PARTICLE_TAG}
 PARTICLE_VERSION=${PARTICLE_TAG:1} # remove the 'v' prefix
 
-MY_DIR=$(dirname "$(readlink -f "$0")")
-git submodule update --init "$MY_DIR/../app/brewblox/proto"
+ROOT_DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
+git submodule update --init "$ROOT_DIR/app/brewblox/proto"
 
-echo "proto_version=$(cd "$MY_DIR/../app/brewblox/proto"; git rev-parse --short HEAD)"
+echo "proto_version=$(cd "$ROOT_DIR/app/brewblox/proto"; git rev-parse --short HEAD)"
 
-OUT_DIR="./firmware-bin/source"
-
-rm -rf "${OUT_DIR}" || true
-mkdir "${OUT_DIR}"
+OUT_DIR="${ROOT_DIR}/docker/firmware-bin/source"
+mkdir -p "${OUT_DIR}"
 
 curl -fL -o "${OUT_DIR}"/bootloader-p1.bin "${PARTICLE_RELEASES}/p1-bootloader@${PARTICLE_VERSION}+lto.bin"
 curl -fL -o "${OUT_DIR}"/system-part1-p1.bin "${PARTICLE_RELEASES}/p1-system-part1@${PARTICLE_VERSION}.bin"
@@ -27,7 +25,7 @@ curl -fL -o "${OUT_DIR}"/system-part2-photon.bin "${PARTICLE_RELEASES}/photon-sy
     echo "[FIRMWARE]",
     echo "firmware_version=$(git rev-parse --short HEAD)",
     echo "firmware_date=$(git show -s --format=%ci)",
-    echo "proto_version=$(cd "$MY_DIR"/../app/brewblox/proto; git rev-parse --short HEAD)",
-    echo "proto_date=$(cd "$MY_DIR"/../app/brewblox/proto; git show -s --format=%ci)",
+    echo "proto_version=$(cd "$ROOT_DIR"/app/brewblox/proto; git rev-parse --short HEAD)",
+    echo "proto_date=$(cd "$ROOT_DIR"/app/brewblox/proto; git show -s --format=%ci)",
     echo "system_version=${PARTICLE_VERSION}"
-} > "${OUT_DIR}"/firmware.ini
+} | tee "${OUT_DIR}"/firmware.ini
