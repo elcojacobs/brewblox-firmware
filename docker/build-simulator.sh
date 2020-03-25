753dd20ee652880eea4b853251e8838824eea9d7
Bob Steers <steers.bob@gmail.com>
2020-03-25 22:23:24 +0100

    add prepare scripts to sim build

--- docker/build-simulator.sh
+++ docker/build-simulator.sh
@@ -3,16 +3,19 @@ set -e
 
 pushd "$(dirname "$(readlink -f "$0")")" > /dev/null
 
+bash ./enable-experimental.sh
+bash ./prepare-buildx.sh
+bash ./start-compiler.sh
+
 IMAGE=brewblox/firmware-simulator
 SRC=firmware-simulator/source
 
 mkdir -p ../build/target
 sudo chown -R "$USER" ../build/target
+rm -rf build/target/*
 
 mkdir -p $SRC
 rm -rf ${SRC:?}/*
-
-bash start-compiler.sh
 
 # Build and copy AMD simulator
 docker-compose exec -T compiler \
@@ -22,10 +25,6 @@ docker-compose exec -T compiler \
         bash build-sim-amd.sh
     "
 cp ../build/target/brewblox-gcc/brewblox $SRC/brewblox-amd
-
-# Build and copy ARM simulator
-# Proto was already built by AMD simulator
-bash ../build/build-sim-arm.sh
 
 # Build AMD image
 docker buildx build \
