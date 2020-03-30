d57ec5e0518c6b2d057d7ab03efa91f9df67c3ed
Elco Jacobs <elco@brewpi.com>
2020-03-30 18:50:05 +0200

    WIP refactor logic actuator

--- app/brewblox/blox/ActuatorLogicBlock.h
+++ app/brewblox/blox/ActuatorLogicBlock.h
@@ -1,26 +1,141 @@
 #pragma once
 
-#include "ActuatorLogic.h"
+#include "ActuatorDigitalConstrained.h"
 #include "BrewBlox.h"
-#include "DigitalActuatorBlock.h"
+#include "ProcessValue.h"
 #include "blox/Block.h"
 #include "cbox/CboxPtr.h"
 #include "proto/cpp/ActuatorLogic.pb.h"
+
+class DigitalCompare {
+public:
+    DigitalCompare(const blox_ActuatorLogic_DigitalCompare& data, cbox::ObjectContainer& objects)
+        : lookup(objects, cbox::obj_id_t(data.id))
+        , op(data.op)
+        , result(blox_ActuatorLogic_Result_FALSE)
+        , rhs(ActuatorDigitalBase::State(data.rhs))
+    {
+    }
+
+    ~DigitalCompare() = default;
+
+    blox_ActuatorLogic_Result eval() const
+    {
+        if (auto actPtr = lookup.const_lock()) {
+            switch (op) {
+            case blox_ActuatorLogic_DigitalCompareOp_VALUE_IS:
+                return blox_ActuatorLogic_Result(actPtr->state() == rhs);
+            case blox_ActuatorLogic_DigitalCompareOp_VALUE_ISNOT:
+                return blox_ActuatorLogic_Result(actPtr->desiredState() != rhs);
+            case blox_ActuatorLogic_DigitalCompareOp_DESIRED_IS:
+                return blox_ActuatorLogic_Result(actPtr->desiredState() == rhs);
+            case blox_ActuatorLogic_DigitalCompareOp_DESIRED_ISNOT:
+                return blox_ActuatorLogic_Result(actPtr->desiredState() != rhs);
+            }
+            return blox_ActuatorLogic_Result_INVALID_DIGITAL_OP;
+        }
+        return blox_ActuatorLogic_Result_BLOCK_NOT_FOUND;
+    }
+
+    void write(blox_ActuatorLogic_DigitalCompare& dest, bool includeNotPersisted) const
+    {
+        dest.id = lookup.getId();
+        dest.op = op;
+        dest.rhs = blox_DigitalState(rhs);
+        if (includeNotPersisted) {
+            dest.result = result;
+        }
+    }
+
+    void update()
+    {
+        result = eval();
+    }
+
+private:
+    cbox::CboxPtr<ActuatorDigitalConstrained> lookup;
+    blox_ActuatorLogic_DigitalCompareOp op;
+    blox_ActuatorLogic_Result result;
+    ActuatorDigitalBase::State rhs;
+};
+
+class AnalogCompare {
+public:
+    AnalogCompare(const blox_ActuatorLogic_AnalogCompare& data, cbox::ObjectContainer& objects)
+        : lookup(objects, cbox::obj_id_t(data.id))
+        , op(data.op)
+        , result(blox_ActuatorLogic_Result_FALSE)
+        , rhs(cnl::wrap<fp12_t>(data.rhs))
+    {
+    }
+
+    ~AnalogCompare() = default;
+
+    blox_ActuatorLogic_Result eval() const
+    {
+        if (auto pvPtr = lookup.const_lock()) {
+            switch (op) {
+            case blox_ActuatorLogic_AnalogCompareOp_VALUE_LE:
+                if (!pvPtr->valueValid()) {
+                    return blox_ActuatorLogic_Result_FALSE;
+                }
+                return blox_ActuatorLogic_Result(pvPtr->value() <= rhs);
+            case blox_ActuatorLogic_AnalogCompareOp_VALUE_GE:
+                if (!pvPtr->valueValid()) {
+                    return blox_ActuatorLogic_Result_FALSE;
+                }
+                return blox_ActuatorLogic_Result(pvPtr->value() >= rhs);
+            case blox_ActuatorLogic_AnalogCompareOp_SETTING_LE:
+                if (!pvPtr->settingValid()) {
+                    return blox_ActuatorLogic_Result_FALSE;
+                }
+                return blox_ActuatorLogic_Result(pvPtr->setting() <= rhs);
+            case blox_ActuatorLogic_AnalogCompareOp_SETTING_GE:
+                if (!pvPtr->settingValid()) {
+                    return blox_ActuatorLogic_Result_FALSE;
+                }
+                return blox_ActuatorLogic_Result(pvPtr->setting() >= rhs);
+            }
+            return blox_ActuatorLogic_Result_INVALID_ANALOG_OP;
+        }
+        return blox_ActuatorLogic_Result_BLOCK_NOT_FOUND;
+    }
+
+    void write(blox_ActuatorLogic_AnalogCompare& dest, bool includeNotPersisted) const
+    {
+        dest.id = lookup.getId();
+        dest.op = op;
+        dest.rhs = cnl::unwrap(rhs);
+        if (includeNotPersisted) {
+            dest.result = result;
+        }
+    }
+
+    void update()
+    {
+        result = eval();
+    }
+
+private:
+    cbox::CboxPtr<ProcessValue<fp12_t>> lookup;
+    blox_ActuatorLogic_AnalogCompareOp op;
+    blox_ActuatorLogic_Result result;
+    fp12_t rhs;
+};
 
 class ActuatorLogicBlock : public Block<BrewBloxTypes_BlockType_ActuatorLogic> {
 private:
     cbox::ObjectContainer& objectsRef; // remember object container reference to create constraints
     cbox::CboxPtr<ActuatorDigitalConstrained> target;
-    ActuatorLogic logic;
     bool enabled = false;
-    std::vector<cbox::CboxPtr<ActuatorDigitalConstrained>> actuatorLookups;
-    std::vector<cbox::CboxPtr<ProcessValue<fp12_t>>> processValueLookups;
+    std::vector<DigitalCompare> digitals;
+    std::vector<AnalogCompare> analogs;
+    std::vector<blox_ActuatorLogic_SyntaxSymbol> symbols;
 
 public:
     ActuatorLogicBlock(cbox::ObjectContainer& objects)
         : objectsRef(objects)
         , target(objects)
-        , logic(target.lockFunctor())
     {
     }
     virtual ~ActuatorLogicBlock() = default;
@@ -32,93 +147,49 @@ public:
         if (result == cbox::CboxError::OK) {
             target.setId(newData.targetId);
             enabled = newData.enabled;
-            logic.clear();
-            actuatorLookups.clear();
-            processValueLookups.clear();
-            // first create all lookups to avoid realloating the vector after creating the functors
-            // This would invalidate the this pointer passed to std::bind in the created functor
-            for (pb_size_t s = 0; s < newData.section_count; s++) {
-                auto msgSection = newData.section[s];
-                if (msgSection.which_section == blox_ActuatorLogic_Section_actuators_tag) {
-                    for (pb_size_t i = 0; i < msgSection.section.actuators.actuator_count; i++) {
-                        cbox::obj_id_t id = msgSection.section.actuators.actuator[i].id;
-                        actuatorLookups.emplace_back(objectsRef, id);
-                    }
-                } else if (msgSection.which_section == blox_ActuatorLogic_Section_comparison_tag) { // compare section
-                    cbox::obj_id_t id = msgSection.section.comparison.compared;
-                    processValueLookups.emplace_back(objectsRef, id);
-                }
+            digitals.clear();
+            analogs.clear();
+
+            for (pb_size_t i = 0; i < newData.digital_count; i++) {
+                digitals.emplace_back(newData.digital[i], objectsRef);
             }
-            // Lookup order stays in sync, is flattened version of all actuator lookups used by logic actuator sections
-            auto actuatorLookupsIt = actuatorLookups.cbegin();
-            auto processValueLookupsIt = processValueLookups.cbegin();
-            for (pb_size_t s = 0; s < newData.section_count; s++) {
-                auto msgSection = newData.section[s];
-                if (msgSection.which_section == blox_ActuatorLogic_Section_actuators_tag) {
-                    auto newSection = std::make_unique<ADLogic::ActuatorSection>(
-                        ADLogic::SectionOp(msgSection.sectionOp),
-                        ADLogic::CombineOp(msgSection.combineOp));
-                    for (pb_size_t i = 0; i < msgSection.section.actuators.actuator_count; i++) {
-                        bool invert = msgSection.section.actuators.actuator[i].invert;
-                        newSection->add(actuatorLookupsIt->lockFunctor(), invert);
-                        actuatorLookupsIt++;
-                    }
-                    logic.addSection(std::move(newSection));
-                } else if (msgSection.which_section == blox_ActuatorLogic_Section_comparison_tag) {
-                    auto newSection = std::make_unique<ADLogic::CompareSection>(
-                        ADLogic::SectionOp(msgSection.sectionOp),
-                        ADLogic::CombineOp(msgSection.combineOp),
-                        processValueLookupsIt->lockFunctor(),
-                        msgSection.section.comparison.useSetting,
-                        cnl::wrap<fp12_t>(msgSection.section.comparison.threshold));
-                    processValueLookupsIt++;
-                    logic.addSection(std::move(newSection));
-                }
+            for (pb_size_t i = 0; i < newData.analog_count; i++) {
+                analogs.emplace_back(newData.analog[i], objectsRef);
+            }
+
+            for (pb_size_t i = 0; i < newData.symbols_count; i++) {
+                symbols.emplace_back(symbols[i]);
             }
         }
         return result;
     }
 
     void
-    writeMessage(blox_ActuatorLogic& message) const
+    writeMessage(blox_ActuatorLogic& message, bool includeNotPersisted) const
     {
         message.targetId = target.getId();
         message.enabled = enabled;
-        message.result = blox_DigitalState(logic.result());
-        // vector lookups stay in sync, are flattened version of nested lookups in same order
-        auto actuatorLookupsIt = actuatorLookups.cbegin();
-        auto processValueLookupsIt = processValueLookups.cbegin();
-        for (const auto& section : logic.sectionsList()) {
-            auto& msgSection = message.section[message.section_count];
-            msgSection.sectionOp = blox_ActuatorLogic_SectionOp(section->sectionOp());
-            msgSection.combineOp = blox_ActuatorLogic_CombineOp(section->combineOp());
+        // message.result = blox_DigitalState(logic.result());
 
-            if (auto ptr = section->asActuatorSection()) {
-                msgSection.which_section = blox_ActuatorLogic_Section_actuators_tag;
-                for (const auto& act : ptr->actuatorList()) {
-                    auto& msgAct = msgSection.section.actuators.actuator[msgSection.section.actuators.actuator_count];
-                    msgAct.id = actuatorLookupsIt->getId();
-                    msgAct.invert = act.invert;
-                    ++actuatorLookupsIt;
-                    msgSection.section.actuators.actuator_count = msgSection.section.actuators.actuator_count + 1;
-                }
-            } else if (auto ptr = section->asCompareSection()) {
-                msgSection.which_section = blox_ActuatorLogic_Section_comparison_tag;
-                msgSection.section.comparison.compared = processValueLookupsIt->getId();
-                msgSection.section.comparison.useSetting = ptr->useSetting();
-                msgSection.section.comparison.threshold = cnl::unwrap(ptr->threshold());
-                ++processValueLookupsIt;
-            }
-
-            message.section_count = message.section_count + 1;
+        for (pb_size_t i = 0; i < digitals.size() && i < 16; i++) {
+            digitals[0].write(message.digital[i], includeNotPersisted);
         }
+        message.digital_count = digitals.size();
+        for (pb_size_t i = 0; i < analogs.size() && i < 16; i++) {
+            analogs[0].write(message.analog[i], includeNotPersisted);
+        }
+        message.analog_count = analogs.size();
+        for (pb_size_t i = 0; i < digitals.size() && i < 16; i++) {
+            message.symbols[i] = symbols[i];
+        }
+        message.symbols_count = symbols.size();
     }
 
     virtual cbox::CboxError
     streamTo(cbox::DataOut& out) const override final
     {
         blox_ActuatorLogic message = blox_ActuatorLogic_init_zero;
-        writeMessage(message);
+        writeMessage(message, true);
 
         return streamProtoTo(out, &message, blox_ActuatorLogic_fields, blox_ActuatorLogic_size);
     }
@@ -127,8 +198,7 @@ public:
     streamPersistedTo(cbox::DataOut& out) const override final
     {
         blox_ActuatorLogic message = blox_ActuatorLogic_init_zero;
-        writeMessage(message);
-        message.result = blox_DigitalState(0); // result doesn't need to be persisted
+        writeMessage(message, false);
 
         return streamProtoTo(out, &message, blox_ActuatorLogic_fields, blox_ActuatorLogic_size);
     }
@@ -137,7 +207,7 @@ public:
     update(const cbox::update_t& now) override final
     {
         if (enabled) {
-            logic.update();
+            // logic.update();
         }
         return now + 100; // update every 100ms
     }
