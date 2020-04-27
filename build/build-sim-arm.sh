7cbe457b818751f99457200e454f0df0c12967b4
Bob Steers <steers.bob@gmail.com>
2020-04-27 22:13:45 +0200

    adjust script for building ARM sim

--- build/build-sim-arm.sh
+++ build/build-sim-arm.sh
@@ -13,16 +13,16 @@ docker run \
     docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
 
 # remove unsupported arg
-sed -i 's/-m64//g' "$ROOT_DIR/platform/spark/device-os/build/gcc-tools.mk"
+sed -i 's/-m64//g' "${ROOT_DIR}/platform/spark/device-os/build/gcc-tools.mk"
 
 # build
 docker run \
     -it --rm \
-    -v "$ROOT_DIR"/:/firmware/ \
-    brewblox/firmware-compiler:latest \
-    make clean all APP=brewblox PLATFORM=gcc
+    -v "${ROOT_DIR}":/firmware \
+    brewblox/firmware-compiler:arm-latest \
+    make APP=brewblox PLATFORM=gcc
 
 # reset modified file
-pushd "$ROOT_DIR/platform/spark/device-os" > /dev/null
+pushd "${ROOT_DIR}/platform/spark/device-os" > /dev/null
 git checkout -- build/gcc-tools.mk 
 popd > /dev/null
