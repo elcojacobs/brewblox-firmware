5d38b6ee85e788de84860b435d8e6b448a6b23a5
Elco Jacobs <elco@brewpi.com>
2020-04-27 18:12:49 +0200

    nicer quotes in build-arm script

--- build/build-sim-arm.sh
+++ build/build-sim-arm.sh
@@ -2,7 +2,7 @@
 set -e
 
 BUILD_DIR=$(dirname "$(readlink -f "$0")")
-ROOT_DIR="${BUILD_DIR}"/..
+ROOT_DIR="${BUILD_DIR}/.."
 
 # clean target dir to not have amd64 leftovers. Sudo needed because docker has created some files as privileged
 sudo rm -rf "${BUILD_DIR}/target"
@@ -13,7 +13,7 @@ docker run \
     docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
 
 # remove unsupported arg
-sed -i 's/-m64//g' "$ROOT_DIR"/platform/spark/device-os/build/gcc-tools.mk 
+sed -i 's/-m64//g' "$ROOT_DIR/platform/spark/device-os/build/gcc-tools.mk"
 
 # build
 docker run \
@@ -23,6 +23,6 @@ docker run \
     make clean all APP=brewblox PLATFORM=gcc
 
 # reset modified file
-pushd "$ROOT_DIR"/platform/spark/device-os > /dev/null
+pushd "$ROOT_DIR/platform/spark/device-os" > /dev/null
 git checkout -- build/gcc-tools.mk 
 popd > /dev/null
