7ef3874a29f3e73ef0f2e27e0434ae44ae24a575
Bob Steers <steers.bob@gmail.com>
2020-04-28 15:35:05 +0200

    add build-bin-arm.sh script, with usage explanation

--- build/build-sim-arm.sh
+++ build/build-sim-arm.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
 set -e
 
-BUILD_DIR=$(dirname "$(readlink -f "$0")")
-ROOT_DIR="${BUILD_DIR}/.."
+# Use repository root
+pushd "$(dirname "$(readlink -f "$0")")/.."
 
 # clean target dir to not have amd64 leftovers. Sudo needed because docker has created some files as privileged
-sudo rm -rf "${BUILD_DIR}/target"
+sudo rm -rf build/target
 
 docker run \
     --rm \
@@ -13,16 +13,23 @@ docker run \
     docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
 
 # remove unsupported arg
-sed -i 's/-m64//g' "${ROOT_DIR}/platform/spark/device-os/build/gcc-tools.mk"
+sed -i 's/-m64//g' platform/spark/device-os/build/gcc-tools.mk
+
+# Make sure compiler is up-to-date
+docker pull \
+    --platform=linux/arm/v7 \
+    brewblox/firmware-compiler:latest
 
 # build
 docker run \
     -it --rm \
-    -v "${ROOT_DIR}":/firmware \
-    brewblox/firmware-compiler:arm-latest \
+    --platform=linux/arm/v7 \
+    -v "$(pwd)/":/firmware/ \
+    brewblox/firmware-compiler:latest \
     make APP=brewblox PLATFORM=gcc
 
 # reset modified file
-pushd "${ROOT_DIR}/platform/spark/device-os" > /dev/null
+cd platform/spark/device-os
 git checkout -- build/gcc-tools.mk 
-popd > /dev/null
+
+popd
