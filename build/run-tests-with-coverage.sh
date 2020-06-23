daa7c553affac4dfcbacbca2f38913b0fcade96f
Elco Jacobs <elco@brewpi.com>
2020-06-23 22:57:01 +0200

    use gcovr to generate coverage report

--- build/run-tests-with-coverage.sh
+++ build/run-tests-with-coverage.sh
@@ -1,56 +1,35 @@
 #!/bin/bash
-MY_DIR=$(dirname $(readlink -f $0))
+MY_DIR=$(dirname "$(readlink -f "$0")")
 ROOT_DIR=$(dirname "$(readlink -f "$MY_DIR")")
-OUTPUT_DIR="$ROOT_DIR/build/coverage"
 
-mkdir -p "$OUTPUT_DIR"
-
-function run_test() {
-    EXECUTABLE=$(readlink -f "$1")
-    BUILD_DIR=$(dirname "$(readlink -f "$EXECUTABLE")")
-    echo "running test executable:  $1"
-    
-    # Reset lcov counters
-    lcov --zerocounters --directory "$BUILD_DIR" --quiet
-
-    # Run lcov initial
-    lcov --capture --initial --directory "$BUILD_DIR" --output-file "$OUTPUT_DIR/base.info" --quiet
-
-    "$EXECUTABLE"
-
-    lcov --capture --directory "$BUILD_DIR" --output-file "$OUTPUT_DIR/test.info" --quiet
-
-    # combine tracefiles
-    lcov --add-tracefile "$OUTPUT_DIR/base.info" --add-tracefile "$OUTPUT_DIR/test.info" --output-file "$2" --quiet
-    rm "$OUTPUT_DIR/base.info" "$OUTPUT_DIR/test.info"
-}
-
-run_test "$ROOT_DIR/controlbox/build/cbox_test_runner" "$OUTPUT_DIR/controlbox.info"
-run_test "$ROOT_DIR/lib/test/build/lib_test_runner" "$OUTPUT_DIR/lib.info"
-run_test "$ROOT_DIR/app/brewblox/test/build/brewblox_test_runner" "$OUTPUT_DIR/brewblox.info"
-
-lcov --quiet \
- --add-tracefile "$OUTPUT_DIR/lib.info" \
- --add-tracefile "$OUTPUT_DIR/controlbox.info" \
- --add-tracefile "$OUTPUT_DIR/brewblox.info" \
- --output-file "$OUTPUT_DIR/combined_traces.info" 
-
-# filter tracefiles
-lcov --remove "$OUTPUT_DIR/combined_traces.info" \
- '*/boost/*' \
- '/usr/*' \
- "$ROOT_DIR/platform/spark/device-os/*" \
- "$ROOT_DIR/app/brewblox/proto/*" \
- "$ROOT_DIR/lib/cnl/**" \
- -o "$OUTPUT_DIR/filtered_traces.info" --quiet
-
-echo "Generating coverage report"
-mkdir -p "$OUTPUT_DIR/html"
-genhtml --prefix "$ROOT_DIR" "$OUTPUT_DIR/filtered_traces.info" \
- --ignore-errors source \
- --output-directory="$OUTPUT_DIR/html" \
- --quiet \
- --show-details \
- --title "Brewblox firmware unit tests"
+"$ROOT_DIR/controlbox/build/cbox_test_runner"
+"$ROOT_DIR/lib/test/build/lib_test_runner"
+"$ROOT_DIR/app/brewblox/test/build/brewblox_test_runner"
 
 
+mkdir -p "$ROOT_DIR/build/coverage/html"
+if [ "$1" = "html" ]
+then
+    FORMAT="html-details"
+    FORMAT_ARGS='--html-title coverage'
+    OUTPUT="$ROOT_DIR/build/coverage/index.html"
+else
+    FORMAT="xml"
+    FORMAT_ARGS=""
+    OUTPUT="$ROOT_DIR/build/coverage/coverage.xml"
+fi
+
+
+gcovr --root "$ROOT_DIR" \
+  "$ROOT_DIR/controlbox/build/" \
+  "$ROOT_DIR/lib/test/build/" \
+  "$ROOT_DIR/app/brewblox/test/build/" \
+  -e '.*/boost/.*' \
+  -e '^/usr/.*' \
+  -e "$ROOT_DIR/platform/spark/device-os/.*" \
+  -e "$ROOT_DIR/app/brewblox/proto/.*" \
+  -e "$ROOT_DIR/lib/cnl/.*" \
+  --$FORMAT "$OUTPUT" \
+  $FORMAT_ARGS \
+  --delete \
+  --print-summary
