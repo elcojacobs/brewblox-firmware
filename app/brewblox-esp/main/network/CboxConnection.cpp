7c88b87b80d65e705ded85d63d89a671109c31bf
Elco Jacobs <elco@brewpi.com>
2021-05-14 17:56:31 +0200

    wip test code

--- app/brewblox-esp/main/network/CboxConnection.cpp
+++ app/brewblox-esp/main/network/CboxConnection.cpp
@@ -23,8 +23,15 @@ void CboxTcpConnection::do_read()
             if (!ec) {
                 cbox::StreamBufDataIn in_cbox(buffer_in);
                 cbox::StreamBufDataOut out_cbox(buffer_out);
-                box.parseMessage(in_cbox, out_cbox);
-                do_write();
+                ESP_LOGI("CbConn", "message received");
+                char test[] = "Connected!\n";
+                auto len = sizeof(test);
+                std::ostream out(&buffer_out);
+                out << "test\n";
+                buffer_in.consume(bytes_transferred);
+
+                do_write(); // send reply
+                do_read();  // read next
             } else if (ec != asio::error::operation_aborted) {
                 connection_manager.stop(shared_from_this());
             }
@@ -33,19 +40,22 @@ void CboxTcpConnection::do_read()
 
 void CboxTcpConnection::do_write()
 {
-    auto self(shared_from_this());
-    asio::async_write(
-        socket, make_view(buffer_out),
-        [this, self](std::error_code ec, std::size_t) {
-            if (!ec) {
-                // Initiate graceful connection closure.
-                asio::error_code ignored_ec;
-                socket.shutdown(asio::ip::tcp::socket::shutdown_both,
-                                ignored_ec);
-            }
+    ESP_LOGI("CbConn", "Out buffer size %u", buffer_out.size());
+    if (buffer_out.size()) {
+        auto self(shared_from_this());
+        asio::async_write(
+            socket, make_view(buffer_out),
+            [this, self](std::error_code ec, std::size_t bytes_transferred) {
+                if (!ec) {
+                    // Initiate graceful connection closure.
+                    asio::error_code ignored_ec;
+                    socket.shutdown(asio::ip::tcp::socket::shutdown_both,
+                                    ignored_ec);
+                }
 
-            if (ec != asio::error::operation_aborted) {
-                connection_manager.stop(shared_from_this());
-            }
-        });
+                if (ec != asio::error::operation_aborted) {
+                    connection_manager.stop(shared_from_this());
+                }
+            });
+    }
 }
